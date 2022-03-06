375113097ae3092d7313b31bc9af1e03d4f000ea
Zachary Lacey <zlacey1234@gmail.com>
2022-03-06 15:45:36 -0800

    Update jacobian_velocity_propagation.m
    Update to have the Jacobian calculation similar to how we calculate the force propagation now (i.e., asking user for angleType).

--- utils/jacobian/jacobian_velocity_propagation.m
+++ utils/jacobian/jacobian_velocity_propagation.m
@@ -3,20 +3,24 @@ function J = jacobian_velocity_propagation(robot, joints, varargin)
 
 %%
     % Default Options
-    defaultJointValueType = 'numeric';
+    defaultJointValueType = 'symbolic';
     expectedJointValueType = {'symbolic', 'numeric'};
-    
+    expectedAngleTypes = {'deg', 'rad'};
+
     p = inputParser;
     
     addRequired(p, 'robot', @(x)(true));
     addRequired(p, 'joints', @(x)(true));
+    addRequired(p, 'angleType', ...
+        @(x) any(validatestring(x, expectedAngleTypes)));
     addOptional(p, 'jointValueType', defaultJointValueType, ...
         @(x) any(validatestring(x, expectedJointValueType)));
     
-    parse(p, robot, joints, varargin{:});
+    parse(p, robot, joints, angleType, varargin{:});
     
     joint_value_type = ...
         validatestring(p.Results.jointValueType, expectedJointValueType); 
+    angle_type = validatestring(p.Results.angleType, expectedAngleTypes); 
     
     % Unpack the Robot Object
     L = robot.links;
@@ -28,14 +32,12 @@ function J = jacobian_velocity_propagation(robot, joints, varargin)
     dot_joint = sym('q%ddot', [1 number_of_joints], 'real');
     
     if strcmp(joint_value_type, 'numeric')
-        w_i_i = zeros(3, number_of_joints + 1);
-        v_i_i = zeros(3, number_of_joints + 1);
-    elseif strcmp(joint_value_type, 'symbolic')
-        w_i_i = sym(zeros(3, number_of_joints + 1));
-        v_i_i = sym(zeros(3, number_of_joints + 1));
-    else 
-        disp('Error: Not a valid jointValueType');
+        joints_numeric = joints;
+        joints = sym('q%d', [1 number_of_joints], 'real');
     end
+
+    w_i_i = sym(zeros(3, number_of_joints + 1));
+    v_i_i = sym(zeros(3, number_of_joints + 1));
     
     joint_0_wrist_span = 1:1:number_of_joints;
     
@@ -62,7 +64,7 @@ function J = jacobian_velocity_propagation(robot, joints, varargin)
         P_iprev_icurr = T_iprev_icurr_matrix(1:3, 4);
         
         if L(joint_index).isrevolute
-            disp('Revolute')
+%             disp('Revolute')
             w_i_i(:,joint_index + 1) = ...
                 simplify(R_icurr_iprev * w_i_i(:, joint_index) + ...
                 [0; 0; dot_joint(joint_index)]);
@@ -72,7 +74,7 @@ function J = jacobian_velocity_propagation(robot, joints, varargin)
                 (cross(w_i_i(:, joint_index), P_iprev_icurr) + ...
                 v_i_i(:,joint_index)), dot_joint));
         else
-            disp('Prismatic')
+%             disp('Prismatic')
             w_i_i(:,joint_index + 1) = ...
                 simplify(R_icurr_iprev * w_i_i(:, joint_index));
             
@@ -85,20 +87,32 @@ function J = jacobian_velocity_propagation(robot, joints, varargin)
         end
     end
     
-%     w_i_i
-%     v_i_i
-    
     w_N_N = simplify(w_i_i(:,number_of_joints + 1));
     v_N_N = simplify(v_i_i(:,number_of_joints + 1));
     
     [Jv_N_N] = equationsToMatrix(v_N_N, dot_joint);
     [Jw_N_N] = equationsToMatrix(w_N_N, dot_joint);
     
-    J_N_N = [Jv_N_N; Jw_N_N]
+    J_N_N = [Jv_N_N; Jw_N_N];
     
-    jacobian_rotation_0_N = [R_0_N zeros(3,3); zeros(3,3) R_0_N]
+    jacobian_rotation_0_N = [R_0_N zeros(3,3); zeros(3,3) R_0_N];
     
     J_0_N = simplify(jacobian_rotation_0_N * J_N_N);
     
     J = J_0_N;
+    
+    if strcmp(joint_value_type, 'numeric')
+        for joint_index = 1:number_of_joints
+            if L(joint_index).isrevolute
+                if strcmp(angle_type, 'deg')
+                    joints_numeric(joint_index) = ...
+                        deg2rad(joints_numeric(joint_index));
+                end
+            end
+            joint_string = ['q' num2str(joint_index)];
+            J = subs(J, joint_string, joints_numeric(joint_index));
+        end
+
+        J = double(J);
+    end
 end
