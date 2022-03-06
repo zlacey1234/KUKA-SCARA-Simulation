88c03a0d5a28b5a72aea7c855aa1f0c1d70b7ff2
Zachary Lacey <zlacey1234@gmail.com>
2022-03-06 13:57:08 -0800

    Update jacobian_explicit.m
    Changes:
    - Updated function description
    - Now requires the user to specify if the joint value is in degrees or radians via the angleType input. (This aims to ensure that the Jacobian Matrix is calculated correctly)
    
    Bug Fixes:
    - Issue where the Jacobian was not calculated properly since the 'joints' could be provided in degrees and the Jacobian calculation expects the joint angles to be in radian. This is fixed by requiring the user to specify the angleType.
    - Needed to ensure that only Revolute Joints are converted from Degrees --> Radians since Prismatic Joints are measured using distance not angle.

--- utils/jacobian/jacobian_explicit.m
+++ utils/jacobian/jacobian_explicit.m
@@ -1,22 +1,58 @@
-function J = jacobian_explicit(robot, joints, varargin)
-%%
-    
+function J = jacobian_explicit(robot, joints, angleType, varargin)
+%% Function: Jacobian_Explicit
+% Summary: This is a function that iteratively solves for the Kinematic
+%          Jacobian Matrix using the Explicit Method.
+%
+% INPUTS: 
+%   robot: @SerialLink Object created using the Robotics Toolbox by Peter
+%          Corke.
+%
+%   joints: Joint Values 
+%
+%   'angleType', {'deg', 'rad'}:         This allows the user to specify
+%                                        what angle measurements type to
+%                                        return the joint trajectory in. 
+%
+% OPTIONAL INPUTS:
+%   jointValueType, {'symbolic', 'numeric'}: A string that allows the 
+%                                            user to specify whether the 
+%                                            'joints' input is a vector 
+%                                            of symbolic variables (i.e., 
+%                                            joints = [th1, th2, th3, d4]) 
+%                                            or if the joints are actual 
+%                                            joint values. [angleType
+%                                            should not matter if the
+%                                            option is 'symbolic']
+%
+% OUTPUT:
+%   J: A (6 x N) matrix that represents the kinematic Jacobian of the
+%      Serial Robot. N is the number of active joints in the kinematic
+%      serial chain.
+%
+% AUTHOR : ZACHARY LACEY
+% AFFILIATION : UNIVERSITY OF CALIFORNIA, LOS ANGELES
+% EMAIL : zlacey@g.ucla.edu
+%         zlacey1234@gmail.com    
 %%  
     defaultJointValueType = 'numeric';
     expectedJointValueType = {'symbolic', 'numeric'};
+    expectedAngleTypes = {'deg', 'rad'};
     
     p = inputParser;
     
     addRequired(p, 'robot', @(x)(true));
     addRequired(p, 'joints', @(x)(true));
+    addRequired(p, 'angleType', ...
+        @(x) any(validatestring(x, expectedAngleTypes)))
     addOptional(p, 'jointValueType', defaultJointValueType, ...
         @(x) any(validatestring(x, expectedJointValueType)));
     
-    parse(p, robot, joints, varargin{:});
+    parse(p, robot, joints, angleType, varargin{:});
     
     joint_value_type = ...
         validatestring(p.Results.jointValueType, expectedJointValueType);
-    
+    angle_type = validatestring(p.Results.angleType, expectedAngleTypes);
+
     % Unpack the Robot Object
     L = robot.links;
     
@@ -32,6 +68,14 @@ function J = jacobian_explicit(robot, joints, varargin)
     end
     
     joint_0_wrist_span = 1:1:number_of_joints;
+    
+    for joint_index = 1:number_of_joints
+        if L(joint_index).isrevolute
+            if strcmp(angle_type, 'deg')
+                joints(joint_index) = deg2rad(joints(joint_index));
+            end
+        end
+    end
     
     if strcmp(joint_value_type, 'numeric')
         T_0_wrist = robot.A(joint_0_wrist_span,... 
@@ -75,5 +119,4 @@ function J = jacobian_explicit(robot, joints, varargin)
     if strcmp(joint_value_type, 'symbolic')
         J = simplify(J);
     end
-    
 end
