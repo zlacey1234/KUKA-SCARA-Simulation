078364a28e593e2abfe486a644b5866010a9da63
Zachary Lacey <zlacey1234@gmail.com>
2022-03-18 15:33:10 -0700

    Update inverse_homogeneous_matrix.m

--- utils/inverse_homogeneous_matrix.m
+++ utils/inverse_homogeneous_matrix.m
@@ -1,4 +1,5 @@
-function inverse_matrix = inverse_homogeneous_matrix(homogeneous_matrix)
+function inverse_matrix = ...
+    inverse_homogeneous_matrix(homogeneous_matrix, jointType)
 %% Function: Inverse_Homogeneous_Matrix
 % Summary: Takes the inverse of a homogeneous transform matrix.
 %
@@ -17,13 +18,29 @@ function inverse_matrix = inverse_homogeneous_matrix(homogeneous_matrix)
 % EMAIL : zlacey@g.ucla.edu
 %         zlacey1234@gmail.com
 %%  
+    expectedJointTypes = {'symbolic', 'numeric'};
+    
+    p = inputParser;
+    
+    addRequired(p, 'homogeneous_matrix', @(x)(true));
+    addRequired(p, 'jointType', ...
+        @(x)any(validatestring(x, expectedJointTypes)));
+    
+    parse(p, homogeneous_matrix, jointType);
+    
+    joint_type = validatestring(p.Results.jointType, expectedJointTypes);
+    
     % Rotation Matrix 
     R = homogeneous_matrix(1:3, 1:3);
 
     % Translational Vector
     p = homogeneous_matrix(1:3, 4);
     
-    inverse_matrix = zeros(size(homogeneous_matrix));
+    if strcmp(joint_type, 'symbolic')
+        inverse_matrix = sym(zeros(size(homogeneous_matrix)));
+    else
+        inverse_matrix = zeros(size(homogeneous_matrix));
+    end
     
     inverse_matrix(1:3, 1:3) = inv(R);
     inverse_matrix(1:3, 4) = -inv(R)*p;
